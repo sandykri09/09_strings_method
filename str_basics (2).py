0d18ca89867218cd605b167e739e526abb0c9a9e
sandykri09 <115703445+sandykri09@users.noreply.github.com>
2022-10-19 15:57:27 +0530

    Add files via upload

---
+++ str_basics (2).py
@@ -0,0 +1,104 @@
+# print(type('hi'))
+# a='''hi'''
+# print(type(a))
+
+'''hey lets do coding'''
+
+# print('hello'       *    5)
+# print(' hello'       *    5)
+# print('hello '       *    -500)
+
+# print('hi'* 3.7)
+
+# print('hi' * 'hello')
+
+# print(dir(__builtins__))
+
+# a=5
+# b=7
+# print(a+b)
+
+
+# a=5
+# b=7.5
+# print(a+b)
+
+
+# a=5
+# b='hey'
+# print(a+b)
+
+# a=5.7
+# b='hey'
+# print(a+b)
+
+# a='hi'
+# b='hey'
+# print(a+b)
+
+
+# a='6'
+# b='hey'
+# print(a+b)
+
+
+# a='6.8'
+# b='hey'
+# print(a+b)
+
+# a=7
+# print(float(a))
+
+# b=8.56
+# print(int(b))
+
+# c='4'
+# print(int(c))
+# print(float(c))
+
+# d='5.7'
+# print(float(d))
+# w=float(d)
+# print(int(w))
+
+# a=5 ; b=7
+# print(a)
+# print(b)
+# print('%d'%a)
+# print('%d'%b)
+# print(a,b)
+# print('%d %d'%(a,b))
+# print('%d         %d'  %(a,b))
+# print("%i %i"%(b,a))
+# print(float(a))
+# print('%f' %a)
+# print('%.1f' %a)
+# print('%.2f' %a)
+
+# c=8.7 
+# print(int(c))
+# print('%d' %c)
+# print('%i' %c)
+# print(float(c))
+# print('%f'%c)
+
+# d=7.24
+# print('%f'%d)
+# print('%.1f'%d)
+# print('%.2f'%d)
+# print('%.3f'%d)
+
+# g=4 ; h=7.9
+# print('%s'%g)
+# print('%s'%h)
+
+# v='hello'
+# print(v)
+# print('%s'%v)
+
+# course='Python'
+# year=2022
+# day=0.5
+
+'''We are doing Python in 2022 for 0.5 a day'''
+# print('We are doing',course,'in',year,'for',day,'a day')
